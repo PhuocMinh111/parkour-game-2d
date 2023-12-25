956416744cece9662e567b3134d85d1c7a3b04de
PhuocMinh111 <phuocminhlenguyen@gmail.com>
2023-12-25 21:50:16 +0700

    running anim

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Rigidbody2D _rb;
+    private Rigidbody2D _rb;
+    private Animator _Animator;
     public bool _begin;
     public float speed = 5f;
     public float jumpForce = 10f;
     public int jumpStep = 2;
+    private bool _isRunning;
     void Start()
     {
-
+        _rb = GetComponent<Rigidbody2D>();
+        _Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,8 +24,18 @@ public class Player : MonoBehaviour
         if (_begin)
             _rb.velocity = new Vector3(speed, _rb.velocity.y);
 
+        if (_rb.velocity.x != 0)
+        {
+
+            _isRunning = true;
+            _Animator.SetBool("isRunning", _isRunning);
+        }
+
         checkInput();
     }
+
+
+
     private void checkInput()
     {
         if (Input.GetMouseButtonDown(0))
@@ -33,6 +46,9 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (jumpStep == 0) return;
+
+            bool isRunning = _Animator.GetBool("isRunning");
+
             _rb.velocity = new Vector3(speed, jumpForce);
             --jumpStep;
         }
